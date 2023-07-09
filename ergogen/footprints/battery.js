2e931fbf78f294fbea4834fd03880b306b3f627c
daniel <github@dbauer.me>
2023-07-09 22:26:58 +0200

    add mcu+batt labels to back of board

--- ergogen/footprints/battery.js
+++ ergogen/footprints/battery.js
@@ -12,6 +12,9 @@ module.exports = {
         ${''/* pin names */}
         (fp_text user + (at 0 6.3 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
         (fp_text user - (at 0 -6.3 ${p.rot + 90}) (layer F.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user + (at 0 6.3 ${p.rot + 90}) (layer B.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+        (fp_text user - (at 0 -6.3 ${p.rot + 90}) (layer B.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15))))
+
 
         ${''/* pins */}
         (pad 1 thru_hole rect (at 0 7.75 ${p.rot}) (size 1.4 1.4) (drill 0.8) (layers *.Cu *.SilkS *.Mask) ${p.RAW.str})
