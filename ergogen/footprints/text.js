a5bbb85bbbc575423ccc0331df39be1bb0c8cd35
daniel <github@dbauer.me>
2023-07-09 23:57:15 +0200

    add label for left and right hand

--- ergogen/footprints/text.js
+++ ergogen/footprints/text.js
@@ -1,14 +1,15 @@
 module.exports = {
     params: {
         text: '',
-        side: 'F'
+        side: 'F',
+        knockout: false
     },
     body: p => `
         (module lib:text (layer F.Cu) (tedit 648E0265)
 
         ${p.at /* parametric position */}
 
-        (fp_text user "${p.text}" (at 0 0 ${p.rot + 90}) (layer ${p.side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ${p.side === 'F' ? "" : "(justify mirror)"} ))
+        (fp_text user "${p.text}" (at 0 0 ${p.rot + 90}) (layer ${p.side}.SilkS ${p.knockout ? "knockout" : ""}) (effects (font (size 0.8 0.8) (thickness 0.15)) ${p.side === 'F' ? "" : "(justify mirror)"} ))
 
         )
         `
