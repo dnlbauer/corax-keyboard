887b6ca4d1ca7e1d4699cd9e154ac9f28cb5df61
daniel <github@dbauer.me>
2023-07-09 23:48:54 +0200

    text saying where to solder

---
+++ ergogen/footprints/text.js
@@ -0,0 +1,17 @@
+module.exports = {
+    params: {
+        text: '',
+        side: 'F'
+    },
+    body: p => `
+        (module lib:text (layer F.Cu) (tedit 648E0265)
+
+        ${p.at /* parametric position */}
+
+        (fp_text user "${p.text}" (at 0 0 ${p.rot + 90}) (layer ${p.side}.SilkS) (effects (font (size 0.8 0.8) (thickness 0.15)) ${p.side === 'F' ? "" : "(justify mirror)"} ))
+
+        )
+        `
+
+}
+
